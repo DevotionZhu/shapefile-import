4c8b073d9f2a3bbb7f3595377ecb7e15607b2d32
Nicoleta Ianosi <nicoianosi@Nicos-MacBook-Pro.local>
2015-09-30 10:28:04 +0300

    Fix errors from linting

--- py-server/server.py
+++ py-server/server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, Response
 import json
-import os, fnmatch
-import tempfile, shutil
+import os
+import fnmatch
+import tempfile
+import shutil
 import zipfile
 
 from shape_importer.shp2pgsql import shape2pgsql
-# from shape_importer.tab2pgsql import shape2pgsql as ogr2ogr
-# from shape_importer.util import walk2
+from shape_importer.tab2pgsql import shape2pgsql as ogr2ogr
 from db_utils.postgis import geojson_from_table
 from gdal_utils.metadata_finder import get_srid_from_prj, get_encoding_from_dbf
 
@@ -16,90 +17,99 @@ app = Flask(__name__, static_folder=STATIC_FOLDER, static_url_path='')
 app.config.from_envvar('APP_CONFIG_FILE')
 
 CONN_STRING = 'dbname={dbname} user={user} password={password}'.format(
-  dbname = app.config['DB_NAME'],
-  user = app.config['DB_USER'],
-  password = app.config['DB_PASSWORD']
+    dbname=app.config['DB_NAME'],
+    user=app.config['DB_USER'],
+    password=app.config['DB_PASSWORD']
 )
 ALLOWED_EXTENSIONS = 'zip'
 
+
 def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 def get_shp_prj_dbf_files_from_tree(temp_dir):
-  files_to_return = {}
-  patterns = ['[a-zA-Z]*.shp', '[a-zA-Z]*.prj', '[a-zA-Z]*.dbf']
+    files_to_return = {}
+    patterns = ['[a-zA-Z]*.shp', '[a-zA-Z]*.prj', '[a-zA-Z]*.dbf']
 
-  for path, dirs, files in os.walk(os.path.abspath(temp_dir)):
-    for extension in patterns:
-      for filename in fnmatch.filter(files, extension):
-        files_to_return[extension[-3:]] = os.path.join(path, filename)
-
-  return files_to_return
-
-def extract_zip(filestream, temp_dir):
-  if not filestream or not allowed_file(filestream.filename):
-    return
-
-  with zipfile.ZipFile(filestream, 'r') as z:
-    z.extractall(temp_dir)
-    files_to_return = get_shp_prj_dbf_files_from_tree(temp_dir)
+    for path, dirs, files in os.walk(os.path.abspath(temp_dir)):
+        for extension in patterns:
+            for filename in fnmatch.filter(files, extension):
+                files_to_return[extension[-3:]] = os.path.join(path, filename)
 
     return files_to_return
 
+
+def extract_zip(filestream, temp_dir):
+    if not filestream or not allowed_file(filestream.filename):
+        return
+
+    with zipfile.ZipFile(filestream, 'r') as z:
+        z.extractall(temp_dir)
+        files_to_return = get_shp_prj_dbf_files_from_tree(temp_dir)
+
+        return files_to_return
+
+
 def user_from_request(request):
-  username = request.headers.get('X-MyGov-Authentication')
-  return username
+    username = request.headers.get('X-MyGov-Authentication')
+    return username
+
 
 def get_data_from_files(file):
-  temp_dir = tempfile.mkdtemp()
-  files = extract_zip(file, temp_dir)
+    temp_dir = tempfile.mkdtemp()
+    files = extract_zip(file, temp_dir)
 
-  if not files or not files['shp']:
+    if not files or not files['shp']:
+        shutil.rmtree(os.path.abspath(temp_dir))
+        return
+
+    data = {
+        'shape': files['shp']
+    }
+    data['srid'] = get_srid_from_prj(files['prj'] or '')
+    data['encoding'] = get_encoding_from_dbf(files['dbf'] or '')
+
     shutil.rmtree(os.path.abspath(temp_dir))
-    return
 
-  data = {
-    'shape': files['shp']
-  }
-  data['srid'] = get_srid_from_prj(files['prj'] or '')
-  data['encoding'] = get_encoding_from_dbf(files['dbf'] or '')
+    return data
 
-  shutil.rmtree(os.path.abspath(temp_dir))
-
-  return data
 
 def get_geojson(request, data):
-  user_name = user_from_request(request) or 'client_test'
-  table_name = shape2pgsql(app.config, user_name, data['shape'], data['srid'], data['encoding'])
-  geojson_data = geojson_from_table(CONN_STRING, table_name)
+    user_name = user_from_request(request) or 'client_test'
+    table_name = shape2pgsql(
+        app.config, user_name, data['shape'], data['srid'], data['encoding']
+    )
+    geojson_data = geojson_from_table(CONN_STRING, table_name)
 
-  return geojson_data
+    return geojson_data
+
 
 @app.route('/api/import/shp2pgsql', methods=['POST'])
 def import_shapefile_shp2pgsql():
-  if request.method != 'POST':
-    return
+    if request.method != 'POST':
+        return
 
-  data = get_data_from_files(request.files['file'])
+    data = get_data_from_files(request.files['file'])
 
-  if not data or not data['shape']:
-    return
+    if not data or not data['shape']:
+        return
 
-  geojson_data = get_geojson(request, data)
+    geojson_data = get_geojson(request, data)
 
-  return Response(
-    json.dumps([{'data': geojson_data}]), mimetype='application/json')
+    return Response(
+        json.dumps([{'data': geojson_data}]), mimetype='application/json')
 
 
 # TODO: Make this work
 @app.route('/api/import/ogr2ogr')
-def import_shapefile_ogr2ogr(): # pragma: no cover
-  zip_name = '/vagrant/shapefiles/streetshighways.zip'
-  filename = create_shapefile(zip_name)
+def import_shapefile_ogr2ogr():  # pragma: no cover
+    zip_file = '/vagrant/shapefiles/streetshighways.zip'
+    filename = create_shapefile(zip_file)
 
-  ogr2ogr(app.config, filename)
-  return '200'
+    ogr2ogr(app.config, filename)
+    return '200'
 
-if __name__ == '__main__': # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
     app.run(port=4002, host='0.0.0.0', debug=True)
--- py-server/tests/test_server.py
+++ py-server/tests/test_server.py
@@ -6,200 +6,202 @@ from server import *
 from werkzeug.datastructures import FileStorage
 
 
-GEOJSON_DATA_FILE = os.path.join(os.path.dirname(__file__), 'data/other/markers.json')
+GEOJSON_DATA_FILE = os.path.join(
+    os.path.dirname(__file__),
+    'data/other/markers.json'
+)
 
 
 class ServerTest(unittest.TestCase):
 
+    def setUp(self):
+        self.geojson_data = open(GEOJSON_DATA_FILE).read()
+        config_test_file = os.getenv("APP_CONFIG_FILE")
+        if not config_test_file or not config_test_file.endswith('testing.py'):
+            self.skipTest(
+                "Please run `export " +
+                "APP_CONFIG_FILE=/vagrant/shapefile-import/config/testing.py`"
+            )
 
-  def setUp(self):
-    self.geojson_data = open(GEOJSON_DATA_FILE).read()
-    configTestFile = os.getenv("APP_CONFIG_FILE")
-    if not configTestFile or not configTestFile.endswith('testing.py'):
-      self.skipTest("Please run `export APP_CONFIG_FILE=/vagrant/shapefile-import/config/testing.py`")
+    def test_connection_string(self):
+        self.assertEqual(
+            CONN_STRING,
+            "dbname=mygov_test user=mygov_test password=mygov_test"
+        )
 
+    def test_allowed_file_not_zip(self):
+        text_file = allowed_file('aa.txt')
+        self.assertFalse(text_file)
 
-  def test_connection_string(self):
-    self.assertEqual(CONN_STRING, "dbname=mygov_test user=mygov_test password=mygov_test")
+    def test_allowed_file_zip(self):
+        zip_file = allowed_file('aa.zip')
+        self.assertTrue(zip_file)
 
+    def test_get_shp_prj_dbf_files_from_tree_has_shp_dbf_prj(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        files_to_compare = {
+            'shp': 'data/test/streetshighways892015_749_210833.shp',
+            'dbf': 'data/test/streetshighways892015_749_210833.dbf',
+            'prj': 'data/test/streetshighways892015_749_210833.prj'
+        }
 
-  def test_allowed_file_not_zip(self):
-    textFile = allowed_file('aa.txt')
-    self.assertFalse(textFile)
+        files_to_return = get_shp_prj_dbf_files_from_tree(
+            current_dir + '/data/test'
+        )
 
+        self.assertIn(files_to_compare['shp'], files_to_return['shp'])
+        self.assertIn(files_to_compare['dbf'], files_to_return['dbf'])
+        self.assertIn(files_to_compare['dbf'], files_to_return['dbf'])
 
-  def test_allowed_file_zip(self):
-    zipFile = allowed_file('aa.zip')
-    self.assertTrue(zipFile)
+    def test_get_shp_prj_dbf_files_from_tree_has_none(self):
+        files_to_return = get_shp_prj_dbf_files_from_tree('/data/fake')
+        self.assertEqual(files_to_return, {})
 
+    # @TODO other tests
+    # test_get_shp_prj_dbf_files_from_tree_has_shp
+    # test_get_shp_prj_dbf_files_from_tree_has_dbf
+    # test_get_shp_prj_dbf_files_from_tree_has_prj
 
-  def test_get_shp_prj_dbf_files_from_tree_has_shp_dbf_prj(self):
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    files_to_compare = {
-      'shp': 'data/test/streetshighways892015_749_210833.shp',
-      'dbf': 'data/test/streetshighways892015_749_210833.dbf',
-      'prj': 'data/test/streetshighways892015_749_210833.prj'
-    }
+    def test_extract_zip_no_filestream(self):
+        files_to_return = extract_zip(None, '/data/test')
+        self.assertEqual(files_to_return, None)
 
-    files_to_return = get_shp_prj_dbf_files_from_tree(current_dir + '/data/test')
+    def test_extract_zip_no_zip_file(self):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        txt_file = current_dir + '/data/StreetsHighways.txt'
 
-    self.assertIn(files_to_compare['shp'], files_to_return['shp'])
-    self.assertIn(files_to_compare['dbf'], files_to_return['dbf'])
-    self.assertIn(files_to_compare['dbf'], files_to_return['dbf'])
+        file = None
+        with open(txt_file, 'rb') as fp:
+            file = FileStorage(fp)
+            files_to_return = extract_zip(file, current_dir + '/data/test')
+            self.assertEqual(files_to_return, None)
 
+    @mock.patch('server.get_shp_prj_dbf_files_from_tree')
+    def test_extract_zip(self, mock_get_shp_prj_dbf_files_from_tree):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        zip_file = current_dir + '/data/StreetsHighways.zip'
 
-  def test_get_shp_prj_dbf_files_from_tree_has_none(self):
-    files_to_return = get_shp_prj_dbf_files_from_tree('/data/fake')
-    self.assertEqual(files_to_return, {})
+        files_to_return = {
+            'shp': 'data/test/streetshighways892015_749_210833.shp',
+            'dbf': 'data/test/streetshighways892015_749_210833.dbf',
+            'prj': 'data/test/streetshighways892015_749_210833.prj'
+        }
 
+        file = None
+        with open(zip_file, 'rb') as fp:
+            file = FileStorage(fp)
+            mock_get_shp_prj_dbf_files_from_tree.return_value = files_to_return
+            files_to_return = extract_zip(file, current_dir + '/data/test')
 
-  # @TODO other tests
-  # test_get_shp_prj_dbf_files_from_tree_has_shp
-  # test_get_shp_prj_dbf_files_from_tree_has_dbf
-  # test_get_shp_prj_dbf_files_from_tree_has_prj
+            self.assertEqual(files_to_return['shp'], files_to_return['shp'])
+            self.assertEqual(files_to_return['dbf'], files_to_return['dbf'])
+            self.assertEqual(files_to_return['dbf'], files_to_return['dbf'])
 
+    @mock.patch('server.request')
+    def test_user_from_request(self, mock_request):
+        mock_request.headers = {
+            'X-MyGov-Authentication': 'Test User'
+        }
+        user_name = user_from_request(mock_request)
+        self.assertEqual(user_name, 'Test User')
 
-  def test_extract_zip_no_filestream(self):
-    files_to_return = extract_zip(None, '/data/test')
-    self.assertEqual(files_to_return, None)
+    @mock.patch('server.request')
+    def test_user_from_request_none(self, mock_request):
+        mock_request.headers = {
+            'No-MyGov-Authentication': ''
+        }
+        user_name = user_from_request(mock_request)
+        self.assertEqual(user_name, None)
 
+    @mock.patch('server.extract_zip')
+    @mock.patch('server.shutil')
+    @mock.patch('server.tempfile')
+    def test_get_data_from_files_no_files(
+        self, mock_tempfile, mock_shutil, mock_extract_zip
+    ):
+        mock_tempfile.mkdtemp.return_value = '/data/test'
+        mock_shutil.rmtree.return_value = True
+        mock_extract_zip.return_value = None
 
-  def test_extract_zip_no_zip_file(self):
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    txt_file = current_dir + '/data/StreetsHighways.txt'
+        file = '/data/test'
+        data = get_data_from_files(file)
+        self.assertEqual(data, None)
 
-    file = None
-    with open(txt_file, 'rb') as fp:
-      file = FileStorage(fp)
-      files_to_return = extract_zip(file, current_dir + '/data/test')
-      self.assertEqual(files_to_return, None)
+    @mock.patch('server.get_encoding_from_dbf')
+    @mock.patch('server.get_srid_from_prj')
+    @mock.patch('server.extract_zip')
+    @mock.patch('server.shutil')
+    @mock.patch('server.tempfile')
+    def test_get_data_from_files(
+        self, mock_tempfile, mock_shutil, mock_extract_zip,
+        mock_get_srid, mock_get_encoding
+    ):
+        mock_tempfile.mkdtemp.return_value = '/data/test'
+        mock_shutil.rmtree.return_value = True
+        files_to_return = {
+            'shp': 'data/test/streetshighways892015_749_210833.shp',
+            'dbf': 'data/test/streetshighways892015_749_210833.dbf',
+            'prj': 'data/test/streetshighways892015_749_210833.prj'
+        }
+        mock_extract_zip.return_value = files_to_return
+        mock_get_srid.return_value = '4326'
+        mock_get_encoding.return_value = 'LATIN1'
 
+        file = '/data/test'
+        data = get_data_from_files(file)
 
-  @mock.patch('server.get_shp_prj_dbf_files_from_tree')
-  def test_extract_zip(self, mock_get_shp_prj_dbf_files_from_tree):
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    zip_file = current_dir + '/data/StreetsHighways.zip'
+        self.assertEqual(
+            data['shape'], 'data/test/streetshighways892015_749_210833.shp'
+        )
+        self.assertEqual(data['srid'], '4326')
+        self.assertEqual(data['encoding'], 'LATIN1')
 
-    files_to_return = {
-      'shp': 'data/test/streetshighways892015_749_210833.shp',
-      'dbf': 'data/test/streetshighways892015_749_210833.dbf',
-      'prj': 'data/test/streetshighways892015_749_210833.prj'
-    }
+    @mock.patch('server.geojson_from_table')
+    @mock.patch('server.shape2pgsql')
+    @mock.patch('server.user_from_request')
+    def test_get_geojson(self, mock_user, mock_shape2pgsql, mock_geojson):
+        mock_user.return_value = None
+        mock_shape2pgsql.return_value = 'table_test'
+        mock_geojson.return_value = self.geojson_data
+        geojson = get_geojson({}, {'shape': '', 'srid': '', 'encoding': ''})
+        self.assertEqual(geojson, self.geojson_data)
 
-    file = None
-    with open(zip_file, 'rb') as fp:
-      file = FileStorage(fp)
-      mock_get_shp_prj_dbf_files_from_tree.return_value = files_to_return
-      files_to_return = extract_zip(file, current_dir + '/data/test')
+    @mock.patch('server.request')
+    def test_import_shapefile_shp2pgsql_no_method_post(self, mock_request):
+        mock_request.method = 'GET'
+        response = import_shapefile_shp2pgsql()
+        self.assertEqual(response, None)
 
-      self.assertEqual(files_to_return['shp'], files_to_return['shp'])
-      self.assertEqual(files_to_return['dbf'], files_to_return['dbf'])
-      self.assertEqual(files_to_return['dbf'], files_to_return['dbf'])
+    @mock.patch('server.get_data_from_files')
+    @mock.patch('server.request')
+    def test_import_shapefile_shp2pgsql_no_data(
+        self, mock_request, mock_get_data
+    ):
+        mock_request.method = 'POST'
+        mock_get_data.return_value = None
+        response = import_shapefile_shp2pgsql()
+        self.assertEqual(response, None)
 
-  @mock.patch('server.request')
-  def test_user_from_request(self, mock_request):
-    mock_request.headers = {
-      'X-MyGov-Authentication': 'Test User'
-    }
-    user_name = user_from_request(mock_request)
-    self.assertEqual(user_name, 'Test User')
+    @mock.patch('server.get_data_from_files')
+    @mock.patch('server.request')
+    def test_import_shapefile_shp2pgsql_no_shape(
+        self, mock_request, mock_get_data
+    ):
+        mock_request.method = 'POST'
+        mock_get_data.return_value = {}
+        response = import_shapefile_shp2pgsql()
+        self.assertEqual(response, None)
 
-  @mock.patch('server.request')
-  def test_user_from_request_none(self, mock_request):
-    mock_request.headers = {
-      'No-MyGov-Authentication': ''
-    }
-    user_name = user_from_request(mock_request)
-    self.assertEqual(user_name, None)
-
-  @mock.patch('server.extract_zip')
-  @mock.patch('server.shutil')
-  @mock.patch('server.tempfile')
-  def test_get_data_from_files_no_files(self, mock_tempfile, mock_shutil, mock_extract_zip):
-    mock_tempfile.mkdtemp.return_value = '/data/test'
-    mock_shutil.rmtree.return_value = True
-    mock_extract_zip.return_value = None
-
-    file ='/data/test'
-    data = get_data_from_files(file)
-    self.assertEqual(data, None)
-
-
-  @mock.patch('server.get_encoding_from_dbf')
-  @mock.patch('server.get_srid_from_prj')
-  @mock.patch('server.extract_zip')
-  @mock.patch('server.shutil')
-  @mock.patch('server.tempfile')
-  def test_get_data_from_files(self, mock_tempfile, mock_shutil, mock_extract_zip, mock_get_srid, mock_get_encoding):
-    mock_tempfile.mkdtemp.return_value = '/data/test'
-    mock_shutil.rmtree.return_value = True
-    files_to_return = {
-      'shp': 'data/test/streetshighways892015_749_210833.shp',
-      'dbf': 'data/test/streetshighways892015_749_210833.dbf',
-      'prj': 'data/test/streetshighways892015_749_210833.prj'
-    }
-    mock_extract_zip.return_value = files_to_return
-    mock_get_srid.return_value = '4326'
-    mock_get_encoding.return_value = 'LATIN1'
-
-    file ='/data/test'
-    data = get_data_from_files(file)
-
-    self.assertEqual(data['shape'], 'data/test/streetshighways892015_749_210833.shp')
-    self.assertEqual(data['srid'], '4326')
-    self.assertEqual(data['encoding'], 'LATIN1')
-
-  @mock.patch('server.geojson_from_table')
-  @mock.patch('server.shape2pgsql')
-  @mock.patch('server.user_from_request')
-  def test_get_geojson(self, mock_user, mock_shape2pgsql, mock_geojson):
-    mock_user.return_value = None
-    mock_shape2pgsql.return_value = 'table_test'
-    mock_geojson.return_value = self.geojson_data
-    geojson = get_geojson({}, {'shape': '', 'srid': '', 'encoding':''})
-    self.assertEqual(geojson, self.geojson_data)
-
-  @mock.patch('server.request')
-  def test_import_shapefile_shp2pgsql_no_method_post(self, mock_request):
-    mock_request.method = 'GET'
-    response = import_shapefile_shp2pgsql()
-    self.assertEqual(response, None)
-
-  @mock.patch('server.get_data_from_files')
-  @mock.patch('server.request')
-  def test_import_shapefile_shp2pgsql_no_data(self, mock_request, mock_get_data):
-    mock_request.method = 'POST'
-    mock_get_data.return_value = None
-    response = import_shapefile_shp2pgsql()
-    self.assertEqual(response, None)
-
-  @mock.patch('server.get_data_from_files')
-  @mock.patch('server.request')
-  def test_import_shapefile_shp2pgsql_no_shape(self, mock_request, mock_get_data):
-    mock_request.method = 'POST'
-    mock_get_data.return_value = {}
-    response = import_shapefile_shp2pgsql()
-    self.assertEqual(response, None)
-
-  @mock.patch('server.get_geojson')
-  @mock.patch('server.get_data_from_files')
-  @mock.patch('server.request')
-  def test_import_shapefile_shp2pgsql(self, mock_request, mock_get_data, mock_get_geojson):
-    mock_request.method = 'POST'
-    mock_get_data.return_value = {
-      'shape': 'test'
-    }
-    mock_get_geojson.return_value = self.geojson_data
-    response = import_shapefile_shp2pgsql()
-    #@TODO assert
-
-
-
-
-
-
-
-
-
-
-
+    @mock.patch('server.get_geojson')
+    @mock.patch('server.get_data_from_files')
+    @mock.patch('server.request')
+    def test_import_shapefile_shp2pgsql(
+        self, mock_request, mock_get_data, mock_get_geojson
+    ):
+        mock_request.method = 'POST'
+        mock_get_data.return_value = {
+            'shape': 'test'
+        }
+        mock_get_geojson.return_value = self.geojson_data
+        import_shapefile_shp2pgsql()
+        # @TODO assert
